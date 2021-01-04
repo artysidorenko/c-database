747ef532d8d8c6d13cf8b4f64c4bd136ec98aea2
Arty <26905074+artysidorenko@users.noreply.github.com>
2021-01-04 21:39:59 +0000

    enable parent node to update after split

--- spec/database.rb
+++ spec/database.rb
@@ -44,7 +44,7 @@ describe 'database' do
     result = run_script(script)
     expect(result.last(2)).to match_array([
       "db > Executed.",
-      "db > Need to implement updating parent after split",
+      "db > Need to implement splitting internal node",
     ])
   end
 
